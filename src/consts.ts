adc94cccacb28626083830ac14aa674e7be0c10d
spooky-th-ghost <colin@wildheartinteractive.com>
2024-11-14 13:19:27 -0500

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Colin R. Casto';
+export const SITE_DESCRIPTION = 'Technical musings';
